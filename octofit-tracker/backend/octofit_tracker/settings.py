04f8269d476da56796a9c4e67a385f29b8314ff4
pavan013 <74134775+pavan013@users.noreply.github.com>
2025-04-08 05:40:20 +0000

    Revert database engine to djongo in settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -53,7 +53,7 @@ WSGI_APPLICATION = 'octofit_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django',
+        'ENGINE': 'djongo',
         'NAME': 'octofit_db',
         'HOST': 'localhost',
         'PORT': 27017,
