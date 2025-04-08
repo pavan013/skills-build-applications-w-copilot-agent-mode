f6f6f45e8b25b4d98c7c86ed840e948e29f3a422
pavan013 <74134775+pavan013@users.noreply.github.com>
2025-04-08 05:35:56 +0000

    Change database engine from djongo to django in settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -53,7 +53,7 @@ WSGI_APPLICATION = 'octofit_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
+        'ENGINE': 'django',
         'NAME': 'octofit_db',
         'HOST': 'localhost',
         'PORT': 27017,
